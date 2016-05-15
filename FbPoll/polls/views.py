f3982f6a5db2cf8b2d26dee60e73180ef0d5f3dc
suvrat <hiran.suvrat@gmail.com>
2016-05-15 16:22:13 +0530

    iteration

--- FbPoll/polls/views.py
+++ FbPoll/polls/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from .models import Question
-from django.http import get_object_or_404
 
 def index(request):
     questionList = Question.objects.all()
@@ -12,7 +11,10 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, questionId):
